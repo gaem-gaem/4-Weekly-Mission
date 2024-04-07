03ec1696e50bb66d31cddaf365beccbb7469aee0
gaem-gaem <kkmm080399@gmail.com>
2024-04-07 23:23:06 +0900

    Feat: 카드 컴포넌트 구현

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -15,6 +15,7 @@ const Card = () => {
     fetchData();
   }, []);
 
+  // links.createdAt yyyy.mm.dd 형태로 변환
   const createDate = (date: string): string => {
     const newDate = new Date(date);
     const year = newDate.getFullYear();
@@ -24,8 +25,36 @@ const Card = () => {
     return formattedDate;
   };
 
+  // links.imageSource가 정상적인 경로가 아닐 때를 위한 에러처리
   const srcError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
     e.currentTarget.src = "/public/assets/images/card-default.png";
+  };
+
+  const displayCreateAt = (createdAt: string) => {
+    const date = new Date(createdAt);
+    const now = Date.now();
+    const milliSeconds: number = now - date.getTime();
+
+    const seconds = milliSeconds / 1000;
+    const minutes = seconds / 60;
+    const hours = minutes / 60;
+    const days = hours / 24;
+    const months = days / 30;
+    const years = months / 12;
+
+    if (seconds < 60) {
+      return "방금 전";
+    } else if (minutes < 60) {
+      return `${Math.floor(minutes)} minutes ago`;
+    } else if (hours < 24) {
+      return `${Math.floor(hours)} hours ago`;
+    } else if (days < 30) {
+      return `${Math.floor(days)} days ago`;
+    } else if (months < 12) {
+      return `${Math.floor(months)} month ago`;
+    } else {
+      return `${Math.floor(years)} years ago`;
+    }
   };
 
   return (
@@ -45,7 +74,7 @@ const Card = () => {
             />
             <div className={S.descriptionContainer}>
               <p className={`font-13-regular ${S.cardMinutesAgo}`}>
-                10 minutes ago
+                {displayCreateAt(link.createdAt)}
               </p>
               <p className={`font-16-regular ${S.cardText}`}>
                 {link.description}
